60b876fe9ef239fc77e5246bd7218be21b6cc7cf
CsYBot <67687653+CsYBot@users.noreply.github.com>
2021-01-26 20:41:35 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,155 +2,48 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 ini_set('max_execution_time', 300);
-
 error_reporting(E_ALL);
-
 define('OAUTH2_CLIENT_ID', '');
 define('OAUTH2_CLIENT_SECRET', '');
-
 $authorizeURL = 'https://discord.com/api/oauth2/authorize';
 $tokenURL = 'https://discord.com/api/oauth2/token';
 $apiURLBase = 'https://discord.com/api/users/@me';
-
+$url = "undefined";
 session_start();
-
 if(get('action') == 'login') {
 
   $params = array(
     'client_id' => OAUTH2_CLIENT_ID,
-    'redirect_uri' => 'url',
+    'redirect_uri' => $url,
     'response_type' => 'code',
     'scope' => 'identify'
   );
-
-  // Redirect the user to Discord's authorization page
   header('Location: https://discordapp.com/api/oauth2/authorize' . '?' . http_build_query($params));
   die();
 }
-
-
 if(get('code')) {
-
-  // Exchange the auth code for a token
   $token = apiRequest($tokenURL, array(
     "grant_type" => "authorization_code",
     'client_id' => OAUTH2_CLIENT_ID,
     'client_secret' => OAUTH2_CLIENT_SECRET,
-    'redirect_uri' => 'url',
+    'redirect_uri' => $url,
     'code' => get('code')
   ));
   $logout_token = $token->access_token;
   $_SESSION['access_token'] = $token->access_token;
-
-
   header('Location: ' . $_SERVER['PHP_SELF']);
 }
-
 if(session('access_token')) {
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   $user = apiRequest($apiURLBase);
-
-
-
-
-
-
-
-
-
-  //echo '<h4>Welcome, ' . $user->username . '</h4>';
+  echo '<h4>Welcome, ' . $user->username . '</h4>';
     //print_r($user);
- // echo '<p><a href="?action=logout">cikis</a></p>';
-
-
-
-
-
-//header('Location: https://www.csybot.cf/kaydol.php?k='.$user->username.$user->discriminator.'&e='.$user->email.'');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-unset($_SESSION["access_token"]);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  echo '<p><a href="?action=logout">cikis</a></p>';
 } else {
-
-
-header('Location: index.php?action=login');
-
-
+echo "<a href='index.php?action=login'>giriş yap</a>";
 }
-
-
 if(get('action') == 'logout') {
-
 unset($_SESSION["access_token"]);
-
 header('Location: index.php');
-
-
 }
 
 function apiRequest($url, $post=FALSE, $headers=array()) {
